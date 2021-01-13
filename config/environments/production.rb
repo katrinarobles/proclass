959ac2399647d38870adc7dd8371507a649b0ed9
Katrina Robles <katrinarobles@live.com>
2021-01-13 14:24:38 +0100

    added Cloudinary config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
-  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
