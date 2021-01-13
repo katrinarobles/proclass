5ebf7a995be89b4bf47c42b87ffc3464ffffe0a5
Katrina Robles <katrinarobles@live.com>
2021-01-13 11:25:53 +0100

    added photo on courses, validated, styled the cards in the index to include photo

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,9 +2,11 @@ class Course < ApplicationRecord
   belongs_to :user
   has_many :occurences, dependent: :destroy
   has_many :bookings, through: :occurences
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 300 }
+  validates :photo, presence: true
 
   def teacher
     @name = User.find(self.user_id).first_name
