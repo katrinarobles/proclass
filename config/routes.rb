c7f04f0f476f0e451be20d8d3e3386e3de15b3db
Katrina Robles <katrinarobles@live.com>
2021-01-14 18:49:12 +0100

    took out rerouted after login, messing with pagination on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  authenticated :user do
-    root 'pages#dashboard', as: :authenticated_root
-  end
   root to: 'pages#home'
-  get "/home", to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'pages#dashboard'
