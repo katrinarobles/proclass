376f95ea30831b11429336e760af0ae2f5190454
Katrina Robles <katrinarobles@live.com>
2021-01-14 12:02:38 +0100

    made sure links are accessible when not signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
 
-    def show
-      @user = User.find(params[:id])
-      @courses = @user.courses
-    end
+  def show
+    @user = User.find(params[:id])
+    @courses = @user.courses
+  end
 end
